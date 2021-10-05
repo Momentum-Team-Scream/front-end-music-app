26d44f5e8eb8a4850e2138989ffec9dfcc777a8f
sarah-wines <sarah.lodico.wines@gmail.com>
2021-10-05 14:35:53 -0400

    Revert " sets up basic navbar and react-router, changes favicon to music note"
    This reverts commit adae2c96508dabfe549dad3484fbfdc86ad295d1.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
-import { Navbar } from './components/Navbar.js';
 
 export const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/students" component={() => <Navbar />} />
-          <Route path="/mydocs" component={() => <Navbar />} />
-        </Switch>
-      </div>
-    </Router>
-  );
+  return <div>HIIII</div>;
 };
