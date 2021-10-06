a7ec37e10e6f5b64edfa6bfae02225bf0041cfef
mjbiener <michaelbiener@yahoo.com>
2021-10-06 19:31:43 -0400

    erged new reactive navbar

--- src/App.js
+++ src/App.js
@@ -5,22 +5,17 @@ import useLocalStorageState from 'use-local-storage-state';
 import { useState, useEffect } from 'react';
 import { RegisterInstructor } from './components/RegisterInstr';
 import { Login } from './components/Login';
-<<<<<<< HEAD
-import { Navbar } from './components/Navbar.js';
 import { LessonList } from './components/LessonList.js';
 import { LessonDetail } from './components/LessonDetail.js';
-
-=======
 import { Navigation } from './components/Navigation.js';
 import { StudentList } from './components/StudentList.js';
 
 // import { AssignmentForm } from './components/AssignmentForm.js';
->>>>>>> 2d849d08843cfe2e018c75712977da6dc3382b06
+
 
 export const App = () => {
   const [auth, setAuth, { removeItem }] = useLocalStorageState('auth', '');
 
-<<<<<<< HEAD
   // const [auth, setAuth, { removeItem }] = useLocalStorageState('token', '');
   // const [username, setUsername] = useState('');
   // const [user, setUser] = useLocalStorageState('user', {});
@@ -42,34 +37,24 @@ export const App = () => {
   //     console.log(user);
   //   }
   // }, [auth],);
-=======
-  return (
-    <>
-      <Router>
-        <Navigation auth={auth} setAuth={setAuth} clearStorage={removeItem} />
->>>>>>> 2d849d08843cfe2e018c75712977da6dc3382b06
 
   return (
     <Router>
       <div className="App">
-        <Navbar auth={auth} setAuth={setAuth } clearStorage={removeItem} />
+        <Navigation auth={auth} setAuth={setAuth } clearStorage={removeItem} />
         <Switch>
           <Route exact path="/" render={() => auth 
             ? <Redirect to={{ pathname: '/upcoming' }}/> 
               : <Redirect to={{ pathname: '/login' }}/> }
           />
-<<<<<<< HEAD
           <Route path="/login" component={() => <Login auth={auth} setAuth={setAuth} />}/>
           <Route path="/register" component={() => <RegisterInstructor setAuth={setAuth} />}/>
           <Route path="/upcoming" component={() => <LessonList auth={auth} />}/>
           <Route path="/lessons/:pk" component={(pk) => <LessonDetail props={pk} auth={auth} />}/>
-          
-=======
           <Route
             path="/students"
             component={() => <StudentList auth={auth} setAuth={setAuth} />}
           />
->>>>>>> 2d849d08843cfe2e018c75712977da6dc3382b06
         </Switch>
       </div>
     </Router>
