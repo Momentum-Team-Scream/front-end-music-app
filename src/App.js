2732b2dc6ae4fa59cc0fb92a4a56cdc11396af37
andreavaughan <amvaughan16@gmail.com>
2021-10-06 10:53:25 -0400

    added redirect for login

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
+import useLocalStorageState from 'use-local-storage-state';
 import { RegisterInstructor } from './components/RegisterInstr';
 import { Login } from './components/Login';
 import { Navbar } from './components/Navbar.js';
 
 export const App = () => {
-  const [auth, setAuth] = useState('');
+  const [auth, setAuth, {removeItem} ] = useLocalStorageState('auth', '');
 
   return (
     <>
       <Router>
-        <Navbar auth={auth} setAuth={setAuth} />
+        <Navbar auth={auth} setAuth={setAuth} clearStorage={removeItem} />
 
         <Switch>
+          <Route 
+            exact path="/"
+            render={() => auth 
+              ? <div>Lessons go here</div>
+              : <Redirect to={{ pathname: '/login' }}/> }
+          />
           <Route
             path="/login"
             component={() => <Login auth={auth} setAuth={setAuth} />}
