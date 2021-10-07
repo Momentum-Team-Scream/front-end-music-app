964ca2c2d9875987fb92bce869c73872e6934f9f
sarah-wines <sarah.lodico.wines@gmail.com>
2021-10-07 15:33:12 -0400

    assignment form prelim

---
+++ src/components/AssignmentForm.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useHistory, useParams } from 'react-router-dom';
+
+export const AssignmentForm = ({ auth }) => {
+  const [body, setBody] = useState('');
+  const history = useHistory();
+  //   const { pk } = useParams();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('CLICK');
+    axios
+      .post(
+        'https://music-mvp.herokuapp.com/api/note/',
+        {
+          body: 'Practice your scales',
+          lesson: 22,
+          is_assignment: true,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `token 4ae18a4c0843f4cdccb2f76fc5228e59d747e429`,
+          },
+        }
+      )
+      .then((res) => {
+        setBody('');
+      });
+  };
+
+  const handleChange = (inputType, event) => {
+    if (inputType === 'body') {
+      setBody(event.target.value);
+    }
+  };
+  console.log(auth);
+  return (
+    <>
+      <form className="form-group" onSubmit={handleSubmit}>
+        <label for="exampleFormControlTextarea1">Assignment Note</label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+          placeholder="Enter assignment note here"
+          type="text"
+          value={body}
+          onChange={(e) => handleChange('body', e)}
+        ></textarea>
+        <div className="button">
+          <button type="submit" className="btn btn-dark">
+            Send note to student
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,7 +1,43 @@
+import { Table } from 'react-bootstrap';
+import '../styles/StudentList.css';
+import { AssignmentForm } from './AssignmentForm.js';
+
 export const StudentList = ({}) => {
   return (
     <>
-      <p>StudentList</p>
+      <p>Your Students</p>
+      <div className="StudentCont">
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+        <AssignmentForm />
+      </div>
     </>
   );
 };
