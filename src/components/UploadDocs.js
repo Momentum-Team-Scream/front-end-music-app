eb430c66a1d58f66ca121981af0a5d2a652afbfa
sarah-wines <sarah.lodico.wines@gmail.com>
2021-10-14 11:36:15 -0400

    makes basic uploading work

--- src/components/UploadDocs.js
+++ src/components/UploadDocs.js
@@ -2,30 +2,50 @@ import { useRef } from 'react';
 import axios from 'axios';
 
 export const UploadDocs = ({ auth }) => {
-  //   let fileInput = useRef(null);
+  let fileInput = useRef(null);
 
-  //   const submitFileData = () => {
-  //     axios.post(``,
-  //     {title:
-  //      author:} {
-  //       headers: {
-  //         Authorization: token ${auth},
-
-  //   },
-  // }).then(
-  //   ((res) => { axios.patch(`/${res.data.pk}`, fileInput, {
-  //     headers: {
-  //       Authorization: `token ${auth}`,
-  //       'Content-Type': fileInput.type,
-  //       'Content-Disposition': `attachment; filename=${fileInput.current.files[0].name}`,
-  //     },
-  //   }}))
-  // );
+  const submitFileData = () => {
+    axios
+      .post(
+        `https://music-mvp.herokuapp.com/api/documents/`,
+        { title: 'title' },
+        {
+          headers: {
+            Authorization: `token ${auth}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        const file = fileInput.current.files[0];
+        console.log(file);
+        console.log(fileInput);
+        axios
+          .put(
+            `https://music-mvp.herokuapp.com/api/documents/${res.data.pk}/upload/`,
+            { file },
+            {
+              headers: {
+                Authorization: `token ${auth}`,
+                'Content-Type': `${file.type}`,
+                'Content-Disposition': `attachment; filename=${file.name}`,
+              },
+            }
+          )
+          .then((res) => {
+            console.log(res);
+          });
+      });
+  };
 
   return (
     <div>
-      <div>{/* <input ref={fileInput} type="file" id="file-input" /> */}</div>
-      <div>{/* <button onClick={submitFileData}>Submit Data</button> */}</div>
+      <div>
+        <input ref={fileInput} type="file" id="file-input" />
+      </div>
+      <div>
+        <button onClick={submitFileData}>Submit Data</button>
+      </div>
     </div>
   );
 };
