06a06bf60edc1727fdadc8ad38d8d469aad4113f
andreavaughan <amvaughan16@gmail.com>
2021-10-16 12:44:15 -0400

    lessons sorted into arrays based on date

--- src/components/StudentDetail.js
+++ src/components/StudentDetail.js
@@ -6,9 +6,11 @@ import '../styles/studetail.css'
 
 export const StudentDetail = ({auth, props}) => {
     const [student, setStudent] = useState({})
-    const [lessons, setLessons] = useState([])
-    const [name, setName] = useState('')
+    const [upcomingLessons, setUpcomingLessons] = useState([])
+    const [pastLessons, setPastLessons] = useState([])
     const [pk, setPk] = useState('')
+    const date = useState([new Date()]);
+    const today = String(date[0]).slice(0, 16);
 
     useEffect( async () => {
         let isMounted = true
@@ -28,7 +30,6 @@ export const StudentDetail = ({auth, props}) => {
                 if (res.status === 200){
                     console.log(res.data)
                     setStudent(res.data)
-                    setName(res.data.first_name)
                     const pk = res.data.pk
                     return axios.get(`https://music-mvp.herokuapp.com/api/assignments/${pk}/`, {
                         headers: {
@@ -39,7 +40,23 @@ export const StudentDetail = ({auth, props}) => {
                         if (isMounted){
                             if (res.status === 200) {
                                 console.log(res.data)
-                                setLessons(res.data)
+                                console.log(typeof res.data)
+                                const lessons = res.data
+                                let upLessons = []
+                                let paLessons = []
+                                console.log(lessons)
+                                lessons.forEach((lesson) => {
+                                    let lesDate = lesson.lesson_date
+                                    console.log(Date.parse(lesDate))
+                                    if (Date.parse(lesDate) >= Date.parse(today)) {
+                                        upLessons.push(lesson)
+                                    } else 
+                                        paLessons.push(lesson)
+                                })
+                                console.log(upLessons)
+                                console.log(paLessons)
+                                setUpcomingLessons(upLessons)
+                                setPastLessons(paLessons)
                             }
                         }
                     })
@@ -54,6 +71,9 @@ export const StudentDetail = ({auth, props}) => {
 
         }, [pk])
 
+    // console.log(lessons)
+    console.log(date[0])
+    
     return (
         <div>
             <header className="stu-header">
@@ -68,39 +88,48 @@ export const StudentDetail = ({auth, props}) => {
             </header>
             <body className="stu-body col-xxl-12 row flex-lg-row-reverse justify-content-center">
                 <div className="body-item col-lg-6">
+                    <h4>Today is {today}</h4>
                     <Link to="/">
                         <button className="btn btn-general">
                             Create New Lesson
                         </button>
                     </Link>
                 </div>
-                <div className="body-item col-lg-6">
+                {/* <div className="body-item col-lg-6">
+                    <h3>Upcoming Lessons</h3>
                     {lessons.map((lesson, idx) => {
                         return (
-                            <div className="card" key={idx}>
-                                <div className="card-header">
-                                    { lesson.plan ?
-                                        <TextTruncate
-                                            line={1}
-                                            element="span"
-                                            truncateText="…"
-                                            text={lesson.plan}
-                                        /> :
-                                        <p>
-                                            Add your lesson plan...
-                                        </p>
-                                    }
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title">{lesson.lesson_date}</h5>
-                                    <Link to={`/lessons/${lesson.pk}`}> 
-                                        <button className="btn btn-general">View Lesson</button>
-                                    </Link>
-                                </div>
-                            </div>
+                            <>
+                                { lesson.date >= date[0] ? 
+                                    <>
+                                        <div className="card" key={idx}>
+                                            <div className="card-header">
+                                                { lesson.plan ?
+                                                    <TextTruncate
+                                                        line={1}
+                                                        element="span"
+                                                        truncateText="…"
+                                                        text={lesson.plan}
+                                                    /> :
+                                                    <p>
+                                                        Add your lesson plan...
+                                                    </p>
+                                                }
+                                            </div>
+                                            <div className="card-body">
+                                                <h5 className="card-title">{lesson.lesson_date}</h5>
+                                                <Link to={`/lessons/${lesson.pk}`}> 
+                                                    <button className="btn btn-general">View Lesson</button>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </> :
+                                    null
+                                }
+                            </>
                         )
                     })}
-                </div>
+                </div> */}
             </body>
         </div>
     );
