97d93930ed286314ceb4dea9962e2ce2f07d5e4b
sarah-wines <sarah.lodico.wines@gmail.com>
2021-10-17 22:48:20 -0400

    further merge conflict resolution

--- src/components/StudentDashboard.js
+++ src/components/StudentDashboard.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom';
 import { AssignmentList } from './AssignmentList';
-import { LogForm } from './LogForm'
-import { LogList } from './LogList'
-import birdnotesleft from '../birds/birdnotesleft.png'
-import '../styles/studentdash.css'
+import { LogForm } from './LogForm';
+import { LogList } from './LogList';
+import birdnotesleft from '../birds/birdnotesleft.png';
+import '../styles/studentdash.css';
 
-export const StudentDashboard = ({auth, show, setShow}) => {
-    const [user, setUser] = useState('')
-    const history = useHistory();
+export const StudentDashboard = ({ auth, show, setShow }) => {
+  const [user, setUser] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     let isMounted = true;
@@ -29,40 +29,10 @@ export const StudentDashboard = ({auth, show, setShow}) => {
         }
       });
 
-        }, [])
-    
-    return (
-        <>
-            <header className="dash-header">
-                <div className="name-edit-links">
-                    <h2>{user.first_name} {user.last_name}</h2>
-                    <Link to="/profile">
-                        <p>Edit Info</p>
-                    </Link>
-                    <button type="button" class="ms-auto stu-dash-btn btn btn-gray">Contact Instructor</button>
-                </div>
-                <div className="student-info">
-                    <div className="">
-                        <p><i class="bi bi-person-circle general"></i> {user.username}</p>
-                        <p><i class="bi bi-envelope-fill general"></i> {user.email}</p>
-                        <p>Emergency Contact: {user.emergency_contact_name}</p>
-                        <p>Emergency Phone: {user.emergency_contact_phone}</p>
-                    </div>
-                </div>
-            </header>
-            <div className="dash-body col-xxl-12 row flex-lg-row-reverse justify-content-center">
-                <div className="body-item col-lg-6">
-                    <LogForm auth={auth} show={show} setShow={setShow}/>
-                    <LogList auth={auth} />
-                    <img className="bird" src={birdnotesleft} alt="bird"></img>
-                </div>
-                <div className="body-item col-lg-6">
-                    <AssignmentList auth={auth}/>
-                </div>
-            </div>
-        </>
-    );
-}
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   return (
     <>
@@ -89,14 +59,13 @@ export const StudentDashboard = ({auth, show, setShow}) => {
             <p>Emergency Contact: {user.emergency_contact_name}</p>
             <p>Emergency Phone: {user.emergency_contact_phone}</p>
           </div>
-          <div>
-            <HeadphonesBird1 width={75} />
-          </div>
         </div>
       </header>
       <div className="dash-body col-xxl-12 row flex-lg-row-reverse justify-content-center">
         <div className="body-item col-lg-6">
-          <LogForm auth={auth} />
+          <LogForm auth={auth} show={show} setShow={setShow} />
+          <LogList auth={auth} />
+          <img className="bird" src={birdnotesleft} alt="bird"></img>
         </div>
         <div className="body-item col-lg-6">
           <AssignmentList auth={auth} />
