22ccbb1cce98da65bb12a7e1186a7b03bbf62958
sarah-wines <sarah.lodico.wines@gmail.com>
2021-10-19 18:47:27 -0400

    puts student dash in container and adjusts ui

--- src/components/LogForm.js
+++ src/components/LogForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useHistory, useParams } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
+import '../styles/studentdash.css';
 
 export const LogForm = ({ auth, setShow, setModalTitle }) => {
   const [body, setBody] = useState('');
@@ -30,12 +31,12 @@ export const LogForm = ({ auth, setShow, setModalTitle }) => {
         }
       )
       .then((res) => {
-        if (res.status === 201){
+        if (res.status === 201) {
           setShow(true);
-          setModalTitle('Practice recorded!')
+          setModalTitle('Practice recorded!');
           setBody('');
           setTimePracticed('');
-          history.go(0)
+          history.go(0);
         }
       });
   };
@@ -48,13 +49,13 @@ export const LogForm = ({ auth, setShow, setModalTitle }) => {
       setTimePracticed(event.target.value);
     }
   };
-  
+
   return (
     <>
       <form className="form-group" onSubmit={handleSubmit}>
-        <h3>Add a Practice Log</h3>
+        <h3 classname="practiceLogTitle">Add a Practice Log</h3>
         <textarea
-          className="form-control"
+          className="form-control what-did-you-practice"
           id="exampleFormControlTextarea1"
           rows="3"
           placeholder="What did you practice?"
@@ -64,29 +65,32 @@ export const LogForm = ({ auth, setShow, setModalTitle }) => {
         ></textarea>
         <div className="form-group-time">
           <label className="label">How long did you practice?</label>
-          <div className="input-group-text mb-3" onChange={(e) => handleChange('time_practiced', e)}>
-          <Form.Control
-            required
-          as="select"
-          defaultValue={''}
-          onChange={(e) => handleChange('time_practiced', e)}
-          className="input form-control"
-          name="students"
+          <div
+            className="input-group-text mb-3"
+            onChange={(e) => handleChange('time_practiced', e)}
           >
-            <option key="" value=''>
-              click to select time practiced!
-            </option>
-            <option value='15'>15 minutes</option>
-            <option value='20'>20 minutes</option>
-            <option value='30'>30 minutes</option>
-            <option value='60'>1 hour</option>
-            <option value='90'>1.5 hours</option>
-            <option value='120'>2 hours</option>
-            <option value='150'>2.5 hours</option>
-            <option value='180'>3 hours</option>
-            <option value='210'>3.5 hours</option>
-            <option value='240'>4 hours</option>
-          </Form.Control>
+            <Form.Control
+              required
+              as="select"
+              defaultValue={''}
+              onChange={(e) => handleChange('time_practiced', e)}
+              className="input form-control"
+              name="students"
+            >
+              <option key="" value="">
+                click to select time practiced
+              </option>
+              <option value="15">15 minutes</option>
+              <option value="20">20 minutes</option>
+              <option value="30">30 minutes</option>
+              <option value="60">1 hour</option>
+              <option value="90">1.5 hours</option>
+              <option value="120">2 hours</option>
+              <option value="150">2.5 hours</option>
+              <option value="180">3 hours</option>
+              <option value="210">3.5 hours</option>
+              <option value="240">4 hours</option>
+            </Form.Control>
           </div>
         </div>
         <div className="button">
